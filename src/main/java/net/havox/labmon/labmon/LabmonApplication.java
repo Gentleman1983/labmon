c48db1df49e1fd72b8c745c83e0f9547ca8f7718
Gentleman1983 (Christian Otto) <Christian.Otto@haVox-Design.de>
2021-10-11 19:40:53 +0200

    #1 Added simple project structure

---
+++ src/main/java/net/havox/labmon/labmon/LabmonApplication.java
@@ -0,0 +1,13 @@
+package net.havox.labmon.labmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/havox/labmon/labmon/LabmonApplicationTests.java
@@ -0,0 +1,13 @@
+package net.havox.labmon.labmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
