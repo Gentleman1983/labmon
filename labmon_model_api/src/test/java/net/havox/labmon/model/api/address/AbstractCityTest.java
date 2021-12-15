4d50b0aeb63a33628d0524e35f33cbf1468b60c5
Gentleman1983 (Christian Otto) <Christian.Otto@haVox-Design.de>
2021-12-15 10:09:01 +0100

    Added Gerkhin describtion for test class in abstract API tests.

--- labmon_model_api/src/test/java/net/havox/labmon/model/api/address/AbstractCityTest.java
+++ labmon_model_api/src/test/java/net/havox/labmon/model/api/address/AbstractCityTest.java
@@ -46,10 +46,10 @@ public abstract class AbstractCityTest {
 
     /**
      * Tests the modification of the ZIP code.
-     *
-     * Given:
-     * When:
-     * Then:
+     * <p>
+     * Given: An {@link City} instance
+     * When: modifying the ZIP code attribute ({@link City#setZipCode(String)})
+     * Then: than the ZIP code attribute ({@link City#getZipCode()}) should contain the new value
      *
      * @throws Exception
      */
@@ -59,16 +59,25 @@ public abstract class AbstractCityTest {
                 ModelRandomGenerator.ALPHANUMERIC_STRING);
 
         City objectUnderTest = getCity();
+        String oldZipCode = objectUnderTest.getZipCode();
+
+        while (zipCode.equals(oldZipCode)) {
+            zipCode = ModelRandomGenerator.randomString(ModelRandomGenerator.randomIntInRange(4, 8),
+                    ModelRandomGenerator.ALPHANUMERIC_STRING);
+        }
+        Assertions.assertNotEquals(oldZipCode, zipCode);
+
         objectUnderTest.setZipCode(zipCode);
         Assertions.assertEquals(zipCode, objectUnderTest.getZipCode());
+        Assertions.assertNotEquals(oldZipCode, objectUnderTest.getZipCode());
     }
 
     /**
      * Tests the modification of the city name.
-     *
-     * Given:
-     * When:
-     * Then:
+     * <p>
+     * Given: An {@link City} instance
+     * When: modifying the name attribute ({@link City#setName(String)})
+     * Then: than the name attribute ({@link City#getName()}) should contain the new value
      *
      * @throws Exception
      */
@@ -78,16 +87,24 @@ public abstract class AbstractCityTest {
         String name = ModelRandomGenerator.randomString(ModelRandomGenerator.randomIntInRange(1, 50), alphabet);
 
         City objectUnderTest = getCity();
+        String oldName = objectUnderTest.getName();
+
+        while (name.equals(oldName)) {
+            name = ModelRandomGenerator.randomString(ModelRandomGenerator.randomIntInRange(1, 50), alphabet);
+        }
+        Assertions.assertNotEquals(oldName, name);
+
         objectUnderTest.setName(name);
         Assertions.assertEquals(name, objectUnderTest.getName());
+        Assertions.assertNotEquals(oldName, objectUnderTest.getName());
     }
 
     /**
      * Tests the modifications of the country.
-     *
-     * Given:
-     * When:
-     * Then:
+     * <p>
+     * Given: An {@link City} instance
+     * When: modifying the country attribute ({@link City#setCountry(Country)})
+     * Then: than the country attribute ({@link City#getCountry()}) should contain the new value
      *
      * @throws Exception
      */
@@ -96,7 +113,15 @@ public abstract class AbstractCityTest {
         Country country = getCountry();
 
         City objectUnderTest = getCity();
+        Country oldCountry = objectUnderTest.getCountry();
+
+        while (country.equals(oldCountry)) {
+            country = getCountry();
+        }
+        Assertions.assertNotEquals(oldCountry, country);
+
         objectUnderTest.setCountry(country);
         Assertions.assertEquals(country, objectUnderTest.getCountry());
+        Assertions.assertNotEquals(oldCountry, objectUnderTest.getCountry());
     }
 }
